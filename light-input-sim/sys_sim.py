102650ba6039772e6c4216440bc445b12ab473c9
Ricardo Cabrita <ricardocabrita@MacBook-Pro-de-Ricardo.local>
2021-01-27 18:15:00 +0000

    Trying to optimize sys_sim to not consume so much memory

--- light-input-sim/sys_sim.py
+++ light-input-sim/sys_sim.py
@@ -20,69 +20,72 @@ if __name__ == "__main__":
 
     fig1 = plt.figure(1)
 
-    ledUVsmd = ledObject(sample_size,z_pos, x_pos, distribution="box", rotate_led=False)
-    ledUVsmd.dist_to_diff = distance_to_diffuserCenter
-    ledUVsmd.simDiffuserEffect(light_theta, diff_theta)
+    led = ledObject(sample_size,z_pos, x_pos, distribution="box", rotate_led=False)
+    led.dist_to_diff = distance_to_diffuserCenter
+    led.simDiffuserEffect(light_theta, diff_theta)
     print("[checkpoint] Done with diffusor simulation")
-    phcount0, cap_angle0 = ledUVsmd.simPinholeEffect(d1, d2, pinh_rad)
-    entryx0 = ledUVsmd.entry_xpos
-    entryz0 = ledUVsmd.entry_zpos
+    phcount0, cap_angle0 = led.simPinholeEffect(d1, d2, pinh_rad)
+    entryx0 = led.entry_xpos
+    entryz0 = led.entry_zpos
     print("2inch from diffusor, half inch from pinhole - Got {}/{} through pinhole!".format(phcount0, sample_size))
+    n2, bins2, patches2 = plt.hist(led.cut_dif_angle, nbins, density=True, color='r',histtype='step', label="SMD")
 
-    x_pos = 0.7
     light_theta = 7.5
-    ledvis = ledObject(sample_size,z_pos, x_pos, distribution="gauss", rotate_led=True)
-    ledvis.calcLEDRotationMatrixes(distance_to_diffuserCenter)
-    ledvis.simDiffuserEffect(light_theta, diff_theta)
+    led.distribution = "gauss"
+    led.rotate_led = True
+    led.led_x_pos = 0.7
+    led.calcLEDRotationMatrixes(distance_to_diffuserCenter)
+    led.simDiffuserEffect(light_theta, diff_theta)
     print("[checkpoint] Done with diffusor simulation")
-    phcount1, cap_angle1 = ledvis.simPinholeEffect(d1, d2, pinh_rad)
-    entryx1 = ledvis.entry_xpos
-    entryz1 = ledvis.entry_zpos
+    phcount1, cap_angle1 = led.simPinholeEffect(d1, d2, pinh_rad)
+    entryx1 = led.entry_xpos
+    entryz1 = led.entry_zpos
     print("2inch from diffusor, half inch from pinhole - Got {}/{} through pinhole!".format(phcount1, sample_size))
+    n2, bins2, patches2 = plt.hist(led.cut_dif_angle, nbins, density=True, color='g',histtype='step', label="DIP-7.5")
 
     light_theta = 15
-    ledvis2 = ledObject(sample_size,z_pos, x_pos, distribution="gauss", rotate_led=True)
-    ledvis2.calcLEDRotationMatrixes(distance_to_diffuserCenter)
-    ledvis2.simDiffuserEffect(light_theta, diff_theta)
+    led.distribution = "gauss"
+    led.rotate_led = True
+    led.led_x_pos = 0.7
+    led.calcLEDRotationMatrixes(distance_to_diffuserCenter)
+    led.simDiffuserEffect(light_theta, diff_theta)
     print("[checkpoint] Done with diffusor simulation")
-    phcount2, cap_angle2 = ledvis2.simPinholeEffect(d1, d2, pinh_rad)
-    entryx2 = ledvis2.entry_xpos
-    entryz2 = ledvis2.entry_zpos
+    phcount2, cap_angle2 = led.simPinholeEffect(d1, d2, pinh_rad)
+    entryx2 = led2.entry_xpos
+    entryz2 = led2.entry_zpos
     print("2inch from diffusor, half inch from pinhole - Got {}/{} through pinhole!".format(phcount2, sample_size))
+    n2, bins2, patches2 = plt.hist(led.cut_dif_angle, nbins, density=True, color='b',histtype='step', label="DIP-15")
 
-    n2, bins2, patches2 = plt.hist(cap_angle2, nbins, density=True, color='g',histtype='step', label="DIP-15")
-    n2, bins2, patches2 = plt.hist(cap_angle1, nbins, density=True, color='b',histtype='step', label="DIP-7.5")
-    n2, bins2, patches2 = plt.hist(cap_angle0, nbins, density=True, color='r',histtype='step', label="SMD")
     plt.xlabel('degrees')
-    plt.ylabel('rel # photons, n={}'.format(sample_size))
+    plt.ylabel('# photons (normalized)')
     plt.legend()
-    plt.savefig('windowAngle.pdf')
+    plt.savefig('diffAngle.pdf')
 
     fig2 = plt.figure(2)
-    plt.scatter(entryx2, entryz2, s=1, c='g',label="DIP-15")
-    plt.scatter(entryx1, entryz1, s=1, c='b',label="DIP-7.5")
-    plt.scatter(entryx0, entryz0, s=1, c='r',label="SMD")
+    plt.scatter(entryx2, entryz2, s=1.3, c='b',label="DIP-15")
+    plt.scatter(entryx1, entryz1, s=1.3, c='g',label="DIP-7.5")
+    plt.scatter(entryx0, entryz0, s=1.3, c='r',label="SMD")
     plt.xlabel('cm')
     plt.ylabel('cm')
     plt.legend()
     plt.savefig('spotPinhole.pdf')
 
     fig3 = plt.figure(3)
-    n2, bins2, patches2 = plt.hist(ledvis2.cut_dif_angle, nbins, density=True, color='g',histtype='step', label="DIP-15")
-    n2, bins2, patches2 = plt.hist(ledvis.cut_dif_angle, nbins, density=True, color='b',histtype='step', label="DIP-7.5")
-    n2, bins2, patches2 = plt.hist(ledUVsmd.cut_dif_angle, nbins, density=True, color='r',histtype='step', label="SMD")
+    n2, bins2, patches2 = plt.hist(cap_angle2, nbins, density=True, color='b',histtype='step', label="DIP-15")
+    n2, bins2, patches2 = plt.hist(cap_angle1, nbins, density=True, color='g',histtype='step', label="DIP-7.5")
+    n2, bins2, patches2 = plt.hist(cap_angle0, nbins, density=True, color='r',histtype='step', label="SMD")
     plt.xlabel('degrees')
     plt.ylabel('# photons (normalized)')
     plt.legend()
-    plt.savefig('diffAngle.pdf')
+    plt.savefig('windowAngle.pdf')
 
-    fig4 = plt.figure(4)
-    n2, bins2, patches2 = plt.hist(ledvis2.theta*(180/math.pi), nbins, density=True, color='g',histtype='step', label="DIP-15")
-    n2, bins2, patches2 = plt.hist(ledvis.theta*(180/math.pi), nbins, density=True, color='b',histtype='step', label="DIP-7.5")
-    n2, bins2, patches2 = plt.hist(ledUVsmd.theta*(180/math.pi), nbins, density=True, color='r',histtype='step', label="SMD")
-    plt.xlabel('degrees')
-    plt.ylabel('# photons (normalized)')
-    plt.legend()
-    plt.savefig('ledProfiles.pdf')
+    # fig4 = plt.figure(4)
+    # n2, bins2, patches2 = plt.hist(led2.theta*(180/math.pi), nbins, density=True, color='g',histtype='step', label="DIP-15")
+    # n2, bins2, patches2 = plt.hist(led.theta*(180/math.pi), nbins, density=True, color='b',histtype='step', label="DIP-7.5")
+    # n2, bins2, patches2 = plt.hist(led.theta*(180/math.pi), nbins, density=True, color='r',histtype='step', label="SMD")
+    # plt.xlabel('degrees')
+    # plt.ylabel('# photons (normalized)')
+    # plt.legend()
+    # plt.savefig('ledProfiles.pdf')
 
     #plt.show()
